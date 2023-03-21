eb0154ee658554e0f16d3af3a6a4f4829a9038ed
João Carlos Peixoto <joaoclpeixoto@gmail.com>
2023-03-21 21:08:11 +0000

    Replace selectui.py

--- gui/selectui.py
+++ gui/selectui.py
@@ -19,34 +19,37 @@ class Ui_select(object):
         self.SelectMainTitle.setAlignment(QtCore.Qt.AlignCenter)
         self.SelectMainTitle.setObjectName("SelectMainTitle")
         self.SelectMainTitle.setMinimumSize(QtCore.QSize(200, 20))
+
         self.pointcheckBox = QtWidgets.QCheckBox(selectui)
         self.pointcheckBox.setGeometry(QtCore.QRect(80, 50, 70, 17))
         self.pointcheckBox.setObjectName("pointcheckBox")
         self.pointcheckBox.setChecked(True)
         self.pointcheckBox.setStyleSheet("image: url(icons/point-icon.png);")
+
         self.segmentcheckBox = QtWidgets.QCheckBox(selectui)
         self.segmentcheckBox.setGeometry(QtCore.QRect(80, 80, 70, 17))
         self.segmentcheckBox.setObjectName("segmentcheckBox")
         self.segmentcheckBox.setChecked(True)
         self.segmentcheckBox.setStyleSheet("image: url(icons/polyline.png);")
+
         self.patchcheckBox = QtWidgets.QCheckBox(selectui)
         self.patchcheckBox.setGeometry(QtCore.QRect(80, 110, 70, 17))
         self.patchcheckBox.setObjectName("patchcheckBox")
         self.patchcheckBox.setStyleSheet("image: url(icons/patch.png);")
         self.patchcheckBox.setChecked(True)
+
         self.propertiespushButton = QtWidgets.QPushButton(selectui)
         self.propertiespushButton.setAutoDefault(True)
-        self.propertiespushButton.setGeometry(QtCore.QRect(60, 160, 80, 23))
+        self.propertiespushButton.setGeometry(QtCore.QRect(60, 150, 80, 23))
         self.propertiespushButton.setObjectName("propertiespushButton")
+
         self.retranslateUi(selectui)
         QtCore.QMetaObject.connectSlotsByName(selectui)
 
     def retranslateUi(self, selectui):
         _translate = QtCore.QCoreApplication.translate
-        self.SelectMainTitle.setText(
-            _translate("MainWindow", "Select Options"))
-        self.propertiespushButton.setText(
-            _translate("MainWindow", "Properties"))
+        self.SelectMainTitle.setText(_translate("MainWindow", "Select Options"))
+        self.propertiespushButton.setText(_translate("MainWindow", "Properties"))
 
 
 class SelectDisplay(QMainWindow, Ui_select):
