678c9b23a4fb4c630aa9d815bd54837ac459c264
João Carlos Peixoto <joaoclpeixoto@gmail.com>
2023-03-21 21:21:19 +0000

    Replace geocollector.py

--- geocollector.py
+++ geocollector.py
@@ -1,6 +1,11 @@
 from geometry.point import Point
-from geometry.segments.line import Line
-from geometry.segments.polyline import Polyline
+from geometry.curves.line import Line
+from geometry.curves.polyline import Polyline
+from geometry.curves.cubicspline import CubicSpline
+from geometry.curves.circle import Circle
+from geometry.curves.circlearc import CircleArc
+from geometry.curves.ellipse import Ellipse
+from geometry.curves.ellipsearc import EllipseArc
 import math
 
 
@@ -29,6 +34,16 @@ class GeoCollector:
             self.geo = Line()
         elif self.geoType == 'POLYLINE':
             self.geo = Polyline()
+        elif self.geoType == 'CUBICSPLINE':
+            self.geo = CubicSpline()
+        elif self.geoType == 'CIRCLE':
+            self.geo = Circle()
+        elif self.geoType == 'CIRCLEARC':
+            self.geo = CircleArc()
+        elif self.geoType == 'ELLIPSE':
+            self.geo = Ellipse()
+        elif self.geoType == 'ELLIPSEARC':
+            self.geo = EllipseArc()
 
     def endGeoCollection(self):
         self.geo = None
@@ -40,7 +55,7 @@ class GeoCollector:
 
     def isCollecting(self):
         if self.geo is not None:
-            if self.geo.getNumberOfPoints() > 0:
+            if self.geo.getNumberOfCtrlPoints() > 0:
                 return True
         return False
 
@@ -56,13 +71,13 @@ class GeoCollector:
                 return True
         return False
 
-    def insertPoint(self, _x, _y, _tol):
+    def insertPoint(self, _x, _y, _LenAndAng, _tol):
         if self.isCollecting():
             if (abs(_x - self.prevPt.getX()) <= _tol and
                     abs(_y - self.prevPt.getY()) <= _tol):
                 return 0
 
-        self.geo.addPoint(_x, _y)
+        self.geo.addCtrlPoint(_x, _y, _LenAndAng)
         self.prevPt.setCoords(_x, _y)
         return 1
 
@@ -74,10 +89,10 @@ class GeoCollector:
         return self.geo
 
     def getDrawPoints(self):
-        return self.geo.getPointsToDrawPt(self.tempPt)
+        return self.geo.getEquivPolylineCollecting(self.tempPt)
 
     def getPoints(self):
-        return self.geo.getPoints()
+        return self.geo.getCtrlPoints()
 
     def getBoundBox(self):
         if self.geo is None:
@@ -97,7 +112,7 @@ class GeoCollector:
         if not self.geo.isUnlimited():
             return False, _x, _y
 
-        pts = self.geo.getPoints()
+        pts = self.geo.getCtrlPoints()
 
         if len(pts) < 3:
             return False, _x, _y
@@ -119,12 +134,26 @@ class GeoCollector:
         if snap:
             return True, xmin, ymin
         else:
-            xC, yC, dist = self.geo.closestPoint(_x, _y)
-
+            status, clstPt, dist, t, tangVec = self.geo.closestPoint(_x, _y)
+            if not status:
+                return False, _x, _y
             if dist < dmin:
                 dmin = dist
-                xmin = xC
-                ymin = yC
+                xmin = clstPt.getX()
+                ymin = clstPt.getY()
                 return True, xmin, ymin
             else:
                 return False, _x, _y
+
+    def CurrentNumberOfControlPoints(self):
+        try:
+            NumCtrlPts = self.geo.nPts
+        except:
+            NumCtrlPts = 0
+
+        return NumCtrlPts
+
+    def updateLineEditValues(self, _x, _y, _LenAndAng):
+        v1, v2 = self.geo.updateLineEditValues(_x, _y, _LenAndAng)
+
+        return v1, v2
